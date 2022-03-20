c61fb25e5247623e0e55f6af68aa51e3ea68a9b3
WilRohde <warohde@gmail.com>
2022-03-20 16:15:34 -0400

    checking in contains.js

---
+++ contains.js
@@ -0,0 +1,73 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    trailingNode() {
+        // if the head is nulll the list is empty, return null
+        if (this.head == null) {
+            return null;
+        }
+
+        // iterate through the list and return the last node
+        let this_node = this.head;
+        while (this_node.next != null) {
+            this_node = this_node.next;
+        }
+        // this_node should be the last node in the list
+        return this_node;
+    }
+
+    addNode(value) {
+        let new_node = new Node(value);
+        if (this.head == null) {
+            // this will be the head node
+            this.head = new_node;
+        }
+        else {
+            // if it's not the head, just add new_node as the last node in the list
+            let last_node = this.trailingNode();
+            last_node.next = new_node;
+        }
+    }
+
+    contains(value) {
+        if (this.head == null) {
+            // list is empty so the value isn't there, return false
+            return false;
+        }
+
+        // assume it's not there
+        var result = false;
+        var this_node = this.head;
+        let next_node = this_node.next;
+        while(next_node.next != null) {
+            if (next_node.value == value) {
+                result = true;
+                break;
+            }
+            next_node = next_node.next;
+        }
+        return result;
+    }
+}
+
+let myList = new SLL();
+
+let myNode = new Node(87);
+myList.addNode(myNode);
+
+let myArray = [18,31,41,52,71,82,95,107,108,204,256,721,891];
+for (i = 0; i < myArray.length; i++) {
+    myNode = myList.addNode(myArray[i]);
+}
+
+let myValue = 121;
+console.log(myList.contains(myValue));
